19ddf4068efbdaff2606a40bd0bd81ac895850f4
Mfern10 <mitchellfernandez147@gmail.com>
2024-04-02 13:30:46 +0900

    Added Resume and Link to open resume in new tab

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,21 @@
+import React from "react";
 import Button from "react-bootstrap/Button";
+import Resume from "../assets/2024_03_25_mitchell_fernandez_resume_en.pdf";
 
-function ButtonClick({title}) {
+function ButtonClick({ title }) {
+  const openResumeInNewTab = () => {
+    window.open(Resume, "_blank");
+  };
+
   return (
     <>
-      <Button className="resume-button" variant="dark">{title}</Button>{" "}
+      <Button
+        className="resume-button"
+        variant="dark"
+        onClick={openResumeInNewTab}
+      >
+        {title}
+      </Button>{" "}
     </>
   );
 }
